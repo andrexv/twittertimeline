d5344e5ae12029a19bd07e049238953345f44df7
Andres Visbal <andresvisbal@gmail.com>
2012-03-25 14:35:33 -0500

    Installing Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittertimeline::Application.config.secret_token = 'faf225e66ac582b862c0bc945d6f627281bf9dae468f3f3bc5f9ebeee9074d9c84a6fd3131e55e1677f6b0cc9db610f95f7ebca553f66637ff4128e22d00b203'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Twittertimeline::Application.config.session_store :cookie_store, :key => '_twittertimeline_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Twittertimeline::Application.config.session_store :active_record_store
